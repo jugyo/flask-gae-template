0834752e7c0063630cb141078bf473132bb6f7cd
jugyo <jugyo.org@gmail.com>
2010-08-05 00:32:56 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# if necessary
+# from flask import redirect, url_for, session, request,\
+#   render_template, abort, flash, get_flashed_messages, g, Response\
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    from wsgiref.handlers import CGIHandler
+    CGIHandler().run(app)
