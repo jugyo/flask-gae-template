d9be1c35ee2e4b9ae172d575d3f6b51379cec807
jugyo <jugyo.org@gmail.com>
2010-08-05 22:32:04 +0900

    add @app.route('/')

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ import sys
 sys.path.append('lib')
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 # if necessary
-# from flask import redirect, url_for, session, request,\
+# from flask import redirect, url_for, session,\
 #   abort, flash, get_flashed_messages, g, Response\
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html', name=request.args.get('name'))
 
 if __name__ == '__main__':
     from wsgiref.handlers import CGIHandler
