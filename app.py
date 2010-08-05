f6d70e1efe01f7449160f0054bd0ca4e6b1bad9e
jugyo <jugyo.org@gmail.com>
2010-08-05 21:51:06 +0900

    use 'lib' dir as library's dir

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('lib')
+
 from flask import Flask
 
 # if necessary
--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-lib='flask jinja2 werkzeug'
+mkdir lib
 
-for i in $lib
+libs='flask jinja2 werkzeug'
+
+for i in $libs
 do
   rm -rf $i
 done
 
 easy_install -U Flask
 
-for i in $lib
+for i in $libs
 do
   d=`easy_install -m $i | grep Using | awk '{print $2}'`/$i
-  cp -rf $d .
+  cp -rf $d lib
 done
